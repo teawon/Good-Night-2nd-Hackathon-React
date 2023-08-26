c3a46abb0a3cd7632bf3654dafbc79bab9b28223
teawon <xodnjsvkvk9@gmail.com>
2023-08-26 13:39:39 +0900

    feat : 영화 삭제 기능 구현

--- src/pages/MovieList/components/MovieItem.tsx
+++ src/pages/MovieList/components/MovieItem.tsx
@@ -1,15 +1,39 @@
+import React, { useState } from "react";
 import styled from "styled-components";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { MovieProps } from "utils/types";
 
 interface MovieItemProps {
   movie: MovieProps;
+  deleteAction: (id: number) => void;
 }
 
-const MovieItem = ({ movie }: MovieItemProps) => {
+const MovieItem = ({ movie, deleteAction }: MovieItemProps) => {
+  const navigate = useNavigate();
+  const [showDeletePopup, setShowDeletePopup] = useState(false);
+
+  const handleMovieClick = () => {
+    navigate(`/movie/${movie.id}`);
+  };
+
+  const handleEditClick = (event: React.MouseEvent) => {
+    event.stopPropagation();
+    navigate(`/edit-movie/${movie.id}`);
+  };
+
+  const handleDeleteClick = (event: React.MouseEvent) => {
+    event.stopPropagation();
+    setShowDeletePopup(true);
+  };
+
+  const handleDeleteActoin = () => {
+    deleteAction(movie.id);
+    setShowDeletePopup(false);
+  };
+
   return (
-    <MovieLink to={`/movie/${movie.id}`}>
-      <MovieWrapper>
+    <>
+      <MovieWrapper onClick={handleMovieClick}>
         <MovieTitle>{movie.title}</MovieTitle>
         <MovieDetails>장르: {movie.genre}</MovieDetails>
         <MovieDetails>개봉일: {movie.releaseDate}</MovieDetails>
@@ -17,17 +41,20 @@ const MovieItem = ({ movie }: MovieItemProps) => {
         <MovieDetails>
           {movie.isCurrentlyShowing ? "상영중" : "상영 종료"}
         </MovieDetails>
-        <EditLink to={`/edit-movie/${movie.id}`}>수정</EditLink>
+        <EditButton onClick={handleEditClick}>수정</EditButton>
+        <DeleteButton onClick={handleDeleteClick}>삭제</DeleteButton>
       </MovieWrapper>
-    </MovieLink>
+
+      {showDeletePopup && (
+        <DeletePopup>
+          <p>정말 삭제하시겠습니까?</p>
+          <button onClick={handleDeleteActoin}>예</button>
+          <button onClick={() => setShowDeletePopup(false)}>아니오</button>
+        </DeletePopup>
+      )}
+    </>
   );
 };
-
-const MovieLink = styled(Link)`
-  text-decoration: none;
-  color: inherit;
-  display: block;
-`;
 
 const MovieWrapper = styled.div`
   padding: 10px;
@@ -46,15 +73,49 @@ const MovieDetails = styled.p`
   margin: 5px 0;
 `;
 
-const EditLink = styled(Link)`
+const EditButton = styled.button`
   position: absolute;
   top: 10px;
-  right: 10px;
+  right: 60px;
   padding: 5px 10px;
   background-color: #007bff;
   color: white;
   text-decoration: none;
   border-radius: 3px;
+`;
+
+const DeleteButton = styled.button`
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  padding: 5px 10px;
+  background-color: #ff3333;
+  color: white;
+  border: none;
+  border-radius: 3px;
+  cursor: pointer;
+`;
+
+const DeletePopup = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: #ffffff;
+  padding: 20px;
+  border-radius: 5px;
+  z-index: 1000;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+
+  p {
+    text-align: center;
+    margin-bottom: 20px;
+  }
+
+  button {
+    padding: 10px 20px;
+    margin: 0 10px;
+  }
 `;
 
 export default MovieItem;
--- src/pages/MovieList/index.tsx
+++ src/pages/MovieList/index.tsx
@@ -1,37 +1,60 @@
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import MovieItem from "./components/MovieItem";
 import { MovieProps } from "utils/types";
 import { Link } from "react-router-dom";
 
 const MovieListPage = () => {
-  // TODO : api 연결 필요
-  const MovieDummyData: MovieProps[] = [
-    {
-      id: 1,
-      title: "영화 1",
-      rating: 4.5,
-      releaseDate: "2022-01-01",
-      endDate: "2022-05-01",
-      genre: "액션",
-      isCurrentlyShowing: true,
-    },
-    {
-      id: 2,
-      title: "영화 2",
-      rating: null,
-      releaseDate: "2021-05-20",
-      endDate: "2021-10-20",
-      genre: "로맨스",
-      isCurrentlyShowing: false,
-    },
-  ];
+  const [movies, setMovies] = useState<MovieProps[]>([]);
+
+  const deleteMovie = (id: number) => {
+    const updatedMovies = movies.filter((movie) => movie.id !== id);
+    setMovies(updatedMovies);
+
+    // TODO : 삭제 api 연결필요
+    console.log(id, "삭제");
+  };
+
+  useEffect(() => {
+    // TODO: api 연결 필요
+    const fetchMovies = () => {
+      return new Promise<MovieProps[]>((resolve) => {
+        setTimeout(() => {
+          resolve([
+            {
+              id: 1,
+              title: "영화 1",
+              rating: 4.5,
+              releaseDate: "2022-01-01",
+              endDate: "2022-05-01",
+              genre: "액션",
+              isCurrentlyShowing: true,
+            },
+            {
+              id: 2,
+              title: "영화 2",
+              rating: null,
+              releaseDate: "2021-05-20",
+              endDate: "2021-10-20",
+              genre: "로맨스",
+              isCurrentlyShowing: false,
+            },
+          ]);
+        }, 100);
+      });
+    };
+
+    fetchMovies().then((data) => {
+      setMovies(data);
+    });
+  }, []);
 
   return (
     <MovieListWrapper>
       <Title>영화 목록</Title>
 
-      {MovieDummyData.map((movie) => (
-        <MovieItem key={movie.id} movie={movie} />
+      {movies.map((movie) => (
+        <MovieItem key={movie.id} movie={movie} deleteAction={deleteMovie} />
       ))}
       <AddMovieButton to="/create-movie">영화 추가</AddMovieButton>
     </MovieListWrapper>
