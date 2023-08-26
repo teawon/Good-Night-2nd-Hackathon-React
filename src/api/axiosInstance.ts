c5ba0486aeac47aa74c12dcbaef93d85406b8a2f
teawon <xodnjsvkvk9@gmail.com>
2023-08-26 21:39:43 +0900

    fix : cors에러 수정

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,6 +1,9 @@
 import axios from "axios";
 
-const baseURL = `${process.env.REACT_APP_BASE_URL}/api/${process.env.REACT_APP_API_VERSION}`;
+const baseURL = `/api/${process.env.REACT_APP_API_VERSION}`;
+
+// TODO : cors에러로 인한 url정보 주석처리 + package.json proxy 설정
+//const baseURL = `${process.env.REACT_APP_BASE_URL}/api/${process.env.REACT_APP_API_VERSION}`;
 
 const axiosInstance = axios.create({
   baseURL,
